de0c9f9bf90c184c4535e47e0aaea20e0e4007c6
Brian <brimerica@gmail.com>
2025-06-23 19:36:15 -0500

    Initial Commit for Node backend

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Hello from your simple Express.js app!')
+})
+
+app.get('/api/message', (req, res) => {
+    res.json({message: 'Hello from the express API!'})
+})
+
+app.listen(port, () => {
+    console.log('Express API is running on http://localhost:${port}')
+})
